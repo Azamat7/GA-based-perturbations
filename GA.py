53bcaac2c2acfddea0e6210e35662531a547c1b9
mukhtar <mukhtarkussaiynbekov@gmail.com>
2020-11-22 21:38:22 +0900

    implement crossover() method and add class properties

--- GA.py
+++ GA.py
@@ -5,14 +5,16 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import random
 
-image = None
-image_size = None
-max_percent_changed = 0.1 # 10%
-max_pixels_changed = image_size * max_percent_changed
-change_pixel_by = 1.1
-
 class GA:
     def __init__(self, model, image, threshold):
+        # image: numpy array
+        self.original_image = image
+        self.image_shape = image.shape
+        self.width = self.image_shape[0]
+        self.height = self.image_shape[1]
+        self.max_percent_changed = 0.1 # 10%
+        self.max_pixels_changed = image_size * max_percent_changed
+        self.change_pixel_by = 1.1
         self.population = []
         self.population_size = 100
         pass
@@ -28,14 +30,17 @@ class GA:
         generates new copy of an image with random pixels modified
         """
         new_image = numpy.copy(image)
+        # Main logic lies here. Modification may be required
         for change_count in range(max_pixels_changed):
-            x_coordinate = random.randint(0, 31) # TODO: change 31 to some other variable. 31 is hard coded.
-            y_coordinate = random.randint(0, 31) # TODO: change 31 to some other variable. 31 is hard coded.
+            x_coordinate = random.randint(0, self.width - 1)
+            y_coordinate = random.randint(0, self.height - 1)
             red = min(new_image[x_coordinate][y_coordinate][0] * change_pixel_by, 1.0)
             green = min(new_image[x_coordinate][y_coordinate][1] * change_pixel_by, 1.0)
             blue = min(new_image[x_coordinate][y_coordinate][2] * change_pixel_by, 1.0)
             new_pixel = [red, green, blue]
             new_image[x_coordinate][y_coordinate][:] = new_pixel # not sure if it is correct way of assigning
+        # End of main logic
+        
         return new_image
 
     def initialize_population(self):
@@ -57,6 +62,18 @@ class GA:
         fitness function
         """
         pass
+
+    def get_changed_pixel_coordinates(self, image):
+        """
+        retrieves coordinates of the modified pixels
+        """
+        changed_pixels_coordinates = []
+        for x_coordinate in range(self.width):
+            for y_coordinate in range(self.height):
+                if image[x_coordinate][y_coordinate] != self.original_image[x_coordinate][y_coordinate]:
+                    changed_pixels_coordinates.append((x_coordinate, y_coordinate))
+
+        return changed_pixels_coordinates
     
     def crossover(self, image1, image2):
         """TODO: mukha
@@ -73,6 +90,25 @@ class GA:
         the offspring to the current population.
         """
         offspring = numpy.copy(image)
+
+        # Main logic lies here. Modification may be required
+        first_image_modified_pixels = self.get_changed_pixel_coordinates(image1)
+        second_image_modified_pixels = self.get_changed_pixel_coordinates(image2)
+        for change_count in range(self.max_pixels_changed):
+            first_or_second = random.randint(0, 1)
+            selected_image = None
+            pixel_coordinate = None
+            if first_or_second == 0: # take pixel from first image
+                pixel_coordinate = numpy.random.choice(first_image_modified_pixels)
+                selected_image = image1
+            else: # take pixel from second image
+                pixel_coordinate = numpy.random.choice(second_image_modified_pixels)
+                selected_image = image2
+            x_coordinate = pixel_coordinate[0]
+            y_coordinate = pixel_coordinate[1]
+            offspring[x_coordinate][y_coordinate] = selected_image[x_coordinate][y_coordinate]
+        # End of main logic
+
         self.mutate(offspring)
         return offspring
     
